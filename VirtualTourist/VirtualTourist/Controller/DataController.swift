8e5b35ce0049558a53c3f11b142dbf0374cd7148
Elena Kulakova <kulakovaelena15@gmail.com>
2019-05-20 21:45:29 +0200

    Create DataController singleton.

---
+++ VirtualTourist/VirtualTourist/Controller/DataController.swift
@@ -0,0 +1,48 @@
+//
+//  DataController.swift
+//  VirtualTourist
+//
+//  Created by Darko Kulakov on 2019-05-20.
+//  Copyright © 2019 Elena Kulakova. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class DataController {
+    
+    static let shared = DataController(modelName: "VirtualTourist")
+    
+    
+    let persistentContainer:NSPersistentContainer
+    
+    var viewContext:NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    let backgroundContext:NSManagedObjectContext!
+    
+    init(modelName:String) {
+        persistentContainer = NSPersistentContainer(name: modelName)
+        
+        backgroundContext = persistentContainer.newBackgroundContext()
+    }
+    
+    func configureContexts() {
+        viewContext.automaticallyMergesChangesFromParent = true
+        backgroundContext.automaticallyMergesChangesFromParent = true
+        
+        backgroundContext.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
+        viewContext.mergePolicy = NSMergePolicy.mergeByPropertyStoreTrump
+    }
+    
+    func load(completion: (() -> Void)? = nil) {
+        persistentContainer.loadPersistentStores { storeDescription, error in
+            guard error == nil else {
+                fatalError(error!.localizedDescription)
+            }
+            self.configureContexts()
+            completion?()
+        }
+    }
+}
